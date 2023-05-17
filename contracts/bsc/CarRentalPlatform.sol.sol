d3bcc1ef51a3e252c986366ff04c5ce9d5f70867
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-05-17 23:56:53 +0200

    CarRentalPlatform v-3

--- contracts/bsc/CarRentalPlatform.sol.sol
+++ contracts/bsc/CarRentalPlatform.sol.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/utils/Counters.sol";
 contract CarRentalPlatform {
- 
+ //DATA 
+
+ //Counter
+ using Counters for Counters.Counter;
+ Counters.Counter private _counter;
 }
