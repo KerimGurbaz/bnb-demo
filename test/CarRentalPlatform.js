56c726536ae84a87b5d588693c0459594ad10af3
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-05-24 17:40:23 +0200

    CarRentalPlatform v-4

--- test/CarRentalPlatform.js
+++ test/CarRentalPlatform.js
@@ -35,17 +35,50 @@ contract("CarRentalPlatform", accounts =>{
       await carRentalPlatform.checkOut(1, {from:user1});
 
       const user = await carRentalPlatform.getUser(user1);
-      assert.equal(user.rentedCardID, 1, "User could not check out the car");
+      assert.equal(user.rentedCarId, 1, "User could not check out the car");
     });
 
     it("Checks in  a car", async()=>{
       await carRentalPlatform.addUser("Alice", "Smith", {from:user1});
       await carRentalPlatform.addCar("Tesla Model S", "example url", 10, 50000, {from:owner});
       await carRentalPlatform.checkOut(1, {from:user1});
+      await new Promise((resolve)=>setTimeout(resolve, 600000)); // 1 min
 
+      await carRentalPlatform.checkIn({from:user1});
 
+      const user = await carRentalPlatform.getUser(user1);
+
+      assert.equal(user.rentedCarId, 0, "User could not check in the car");
+      assert.equal(user.debt, 10, "User debt did not get created");
+  });
   });
 
+  describe("Deposit token and make payment", ()=>{
+    it("deposits token", async()=>{
+      await carRentalPlatform.addUser("Alice", "Smith", {from:user1});
+      await carRentalPlatform.deposit({from:user1, value:100}) ;
+     
+      const user = await carRentalPlatform.getUser(user1);
+      assert.equal(user.balance, 100, "User could not deposit tokens");
+    });
+
+    it("makes a payment", async()=>{
+      await carRentalPlatform.addUser("Alice", "Smith", {from:user1});
+      await carRentalPlatform.addCar("Tesla Model S", "example url", 10, 50000, {from:owner});
+      await carRentalPlatform.checkOut(1, {from:user1});
+      await new Promise((resolve)=>setTimeout(resolve, 600000)); // 1 min
+      await carRentalPlatform.checkIn({from:user1});
+
+      await carRentalPlatform.deposit({from:user1, value:100}) ;
+      await carRentalPlatform.makePayment({from:user1}) ;
+
+      const user = await carRentalPlatform.getUser(user1);
+
+      assert.equal(user.debt, 0, "Something went wrong while trying to make the payment");
+     
+  });
+});
 
 
-})
+
+});
